36c7f9a48ce883d6d9372927eaaadaf4d6c12058
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-05-07 23:25:21 +0200

    add auth

---
+++ src/Header.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { signInWithPopup } from "firebase/auth";
+
+import { auth, provider } from "./firebase";
+
+const Header = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      console.log(user);
+      setCurrentUser(user);
+    });
+  }, []);
+
+  const onSignInClick = () => {
+    signInWithPopup(auth, provider);
+  };
+
+  const onSignOut = () => {
+    auth.signOut();
+  };
+
+  return (
+    <header className="px-5 flex justify-end cursor-pointer hover:underline">
+      {currentUser ? (
+        <button onClick={onSignOut}>{currentUser.displayName}</button>
+      ) : (
+        <button onClick={onSignInClick}>Sign In</button>
+      )}
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+import { firebaseConfig } from "./config";
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export const provider = new GoogleAuthProvider();
